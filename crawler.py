75400b9b1c22cab25c349638bd4e3c66cf85f01c
Muhwan Kim <mhkim4886@gmail.com>
2020-11-27 20:46:25 +0900

    Change parsing logic

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ from worker import Worker
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import traceback
 
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
@@ -19,31 +20,30 @@ class Crawler:
 
     def _get_current(self):
         driver.get("https://www.corona-live.com/")
-        try:
-            time.sleep(10)
-            html = driver.page_source
-            soup = BeautifulSoup(html, "html.parser")
-            new_patients = soup.select(
-                "#root > div > div.sc-AxjAm.bRuiNy.sc-AxirZ.fZVwIW.sc-AxiKw.eSbheu > div:nth-child(3) > div.sc-AxjAm.fiWmrI.sc-AxirZ.heBhHx.sc-AxiKw.eSbheu > div.sc-AxjAm.hQNuxH.sc-AxirZ.idrxEV.sc-AxiKw.eSbheu > div.sc-AxjAm.lklBhC > span"
-            )
-            curr = int(new_patients[0].text)
-            return curr
-        except Exception as e:
-            print(e)
-            return self.latest
+        time.sleep(10)
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        div = soup.find_all(name="div", text="실시간 확진자수")
+        curr = int(div[0].parent.contents[1].contents[0].contents[0].text)
+        return curr
 
     async def run(self):
         while True:
-            curr = self._get_current()
-            diff = curr - self.latest
             curr_time = datetime.today().strftime("%Y.%m.%d %H:%M")
-            print(f"{curr_time}: {curr}")
-            await self.worker.test_send(msg=f"{curr_time} HEARTBEAT {curr}")
-            if self.latest < curr:
-                await self.worker.send(msg=f"{curr_time} 확진자 수 변동: {curr} (+{diff})")
-                self.latest = curr
+            try:
+                curr = self._get_current()
+                diff = curr - self.latest
+                print(f"{curr_time}: {curr}")
+                if self.latest < curr:
+                    await self.worker.send(msg=f"{curr_time} 확진자 수 변동: {curr} (+{diff})")
+                    self.latest = curr
+                if self.latest > curr:
+                    await self.worker.send(msg=f"{curr_time} 확진자 수 변동: {curr}")
+                    self.latest = curr
+            except Exception:
+                err = traceback.format_exc()
+                print(err)
+                await self.worker.test_send(msg="@here corona-live-bot error!")
+                await self.worker.test_send(msg=f"{err}")
 
-            if self.latest > curr:
-                await self.worker.send(msg=f"{curr_time} 확진자 수 변동: {curr}")
-                self.latest = curr
             await asyncio.sleep(600)
